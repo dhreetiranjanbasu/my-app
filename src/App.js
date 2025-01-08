5a40eccc13b4c7ec278b2c146af726f6a9063b47
Dhreeti Ranjan Basu <dhreetiranjanbasu@Dhreetis-MacBook-Pro.local>
2025-01-08 13:38:42 +0600

    Firebase added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> I don't know what to put here.
         </p>
         <a
           className="App-link"
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAYHwiLrt6EpBDrRssOPtfeX4sL2G4mJUU",
+  authDomain: "myappfp.firebaseapp.com",
+  projectId: "myappfp",
+  storageBucket: "myappfp.firebasestorage.app",
+  messagingSenderId: "766980052723",
+  appId: "1:766980052723:web:4c9ae7ecbc93951d2c8986",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
